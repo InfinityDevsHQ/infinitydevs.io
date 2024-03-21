7b916a9a3795d755c035e4c0cac124c804fd5538
sheraz-manzoor <sherazmanzoor54@gmail.com>
2024-03-21 18:58:25 +0500

    fix: sidebar background and width

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -56,19 +56,19 @@ export default function Header({ className }: HeaderProps) {
 
           {sidebarOpen && (
             <div className="flex fixed top-0 z-50 right-0 h-screen w-screen">
-              <div className="flex-1 flex justify-center bg-black/40 px-6">
-                <button
-                  aria-label="Sidebar menu close button"
-                  type="button"
-                  className="text-white text-4xl w-full h-16"
-                  onClick={() => setSidebarOpen(false)}
-                >
-                  <X size={32} />
-                </button>
-              </div>
-              <div className="bg-white  h-full w-3/4 md:w-1/2">
-                <nav className="text-zinc-800 text-4xl capitalize font-bold h-full">
-                  <ol className="h-full flex flex-col items-center justify-evenly">
+              <div className="bg-gray-900  h-full w-full">
+                <div className="px-6 py-4">
+                  <button
+                    aria-label="Sidebar menu close button"
+                    type="button"
+                    className="text-white text-4xl w-full"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <X size={32} />
+                  </button>
+                </div>
+                <nav className="flex items-center justify-center h text-zinc-300 text-4xl capitalize font-bold h-full">
+                  <ol className="h-full flex flex-col items-center justify-between">
                     <li>
                       <Link href={"/"}>Home</Link>
                     </li>
