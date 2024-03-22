47077fe85594145990c6a69dc262347ff917e894
sheraz-manzoor <sherazmanzoor54@gmail.com>
2024-03-22 22:05:27 +0500

    feat: added animation to sidebar using frammer motion

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,11 +4,19 @@ import { Menu } from "lucide-react";
 import { X } from "lucide-react";
 import { useState } from "react";
 import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+
 export type HeaderProps = {
   className?: string;
 };
+
 export default function Header({ className }: HeaderProps) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const sidebarVariants = {
+    open: { x: 0 },
+    closed: { x: "100%" },
+  };
+
   return (
     <header
       className={`flex items-center justify-between w-full  px-4 pt-8 ${className}`}
@@ -30,16 +38,16 @@ export default function Header({ className }: HeaderProps) {
             <Link href={"/"}>About Us</Link>
           </li>
           <li>
-            <Link href={"/"}>Insiahts</Link>
+            <Link href={"/"}>Insights</Link>
           </li>
           <li>
             <Link href={"/"}>Portfolio</Link>
           </li>
           <li>
-            <Link href={"/"}>contact us</Link>
+            <Link href={"/"}>Contact Us</Link>
           </li>
           <li>
-            <Link href={"/"}>careers</Link>
+            <Link href={"/"}>Careers</Link>
           </li>
         </ol>
       </nav>
@@ -53,45 +61,52 @@ export default function Header({ className }: HeaderProps) {
           >
             <Menu size={32} />
           </button>
-
-          {sidebarOpen && (
-            <div className="flex fixed top-0 z-50 right-0 h-screen w-screen">
-              <div className="bg-gray-900  h-full w-full">
-                <div className="px-6 py-4">
-                  <button
-                    aria-label="Sidebar menu close button"
-                    type="button"
-                    className="text-white text-4xl w-full"
-                    onClick={() => setSidebarOpen(false)}
-                  >
-                    <X size={32} />
-                  </button>
+          <AnimatePresence>
+            {sidebarOpen && (
+              <motion.div
+                className="flex fixed top-0 z-50 right-0 h-screen w-screen"
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={sidebarVariants}
+              >
+                <div className="bg-gray-900  h-full w-full">
+                  <div className="px-6 py-4">
+                    <button
+                      aria-label="Sidebar menu close button"
+                      type="button"
+                      className="text-white text-4xl w-full"
+                      onClick={() => setSidebarOpen(false)}
+                    >
+                      <X size={32} />
+                    </button>
+                  </div>
+                  <nav className="flex items-center justify-center h text-zinc-300 text-4xl capitalize font-bold h-full">
+                    <ol className="h-full flex flex-col items-center justify-between">
+                      <li>
+                        <Link href={"/"}>Home</Link>
+                      </li>
+                      <li>
+                        <Link href={"/"}>About Us</Link>
+                      </li>
+                      <li>
+                        <Link href={"/"}>Insights</Link>
+                      </li>
+                      <li>
+                        <Link href={"/"}>Portfolio</Link>
+                      </li>
+                      <li>
+                        <Link href={"/"}>Contact Us</Link>
+                      </li>
+                      <li>
+                        <Link href={"/"}>Careers</Link>
+                      </li>
+                    </ol>
+                  </nav>
                 </div>
-                <nav className="flex items-center justify-center h text-zinc-300 text-4xl capitalize font-bold h-full">
-                  <ol className="h-full flex flex-col items-center justify-between">
-                    <li>
-                      <Link href={"/"}>Home</Link>
-                    </li>
-                    <li>
-                      <Link href={"/"}>About Us</Link>
-                    </li>
-                    <li>
-                      <Link href={"/"}>Insiahts</Link>
-                    </li>
-                    <li>
-                      <Link href={"/"}>Portfolio</Link>
-                    </li>
-                    <li>
-                      <Link href={"/"}>contact us</Link>
-                    </li>
-                    <li>
-                      <Link href={"/"}>careers</Link>
-                    </li>
-                  </ol>
-                </nav>
-              </div>
-            </div>
-          )}
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </header>
