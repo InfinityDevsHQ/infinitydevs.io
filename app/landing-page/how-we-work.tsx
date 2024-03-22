1d7fbc9cf77770fc03c18e544e207f526aa7ce75
sheraz-manzoor <sherazmanzoor54@gmail.com>
2024-03-22 14:19:30 +0500

    feat: how we work mobile version

--- app/landing-page/how-we-work.tsx
+++ app/landing-page/how-we-work.tsx
@@ -9,8 +9,8 @@ export default function HowWeWork() {
       >
         <span>Who</span> we are
       </Heading>
-      <div className="flex items-center justify-center w-full box-max-width">
-        <div className="grid grid-cols-3 items-center justify-items-center uppercase text-xl font-bold text-zinc-300">
+      <div className="flex items-center justify-center w-full uppercase text-base sm:text-xl font-bold text-zinc-300">
+        <div className="hidden md:grid grid-cols-3 box-max-width items-center justify-items-center">
           <div className="flex flex-col self-start">
             <h2 className="mt-12">1. Requirement Gathering</h2>
             <br />
@@ -30,6 +30,32 @@ export default function HowWeWork() {
             <h2 className="mt-52">2. Brain Storming</h2>
             <h2 className="mt-80">4. Development</h2>
             <h2 className="mt-[22rem]">6. Deliver to the client</h2>
+          </div>
+        </div>
+        <div className="flex md:hidden flex-col gap-12 max-width-[800px]">
+          <div className="flex justify-between gap-4 items-center">
+            <h3>1. Requirement Gathering</h3>
+            <PlayCircle size={32} />
+          </div>
+          <div className="flex  justify-between items-center">
+            <PlayCircle size={32} />
+            <h3>2. Brain Storming</h3>
+          </div>
+          <div className="flex  justify-between items-center">
+            <h3>3. Prototyping</h3>
+            <PlayCircle size={32} />
+          </div>
+          <div className="flex  justify-between items-center">
+            <PlayCircle size={32} />
+            <h3>4. Development</h3>
+          </div>
+          <div className="flex  justify-between items-center">
+            <h3>5. Testing</h3>
+            <PlayCircle size={32} />
+          </div>
+          <div className="flex  justify-between items-center">
+            <PlayCircle size={32} />
+            <h3>6. Deliver to client</h3>
           </div>
         </div>
       </div>
