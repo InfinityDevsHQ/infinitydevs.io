4e52451b99434203d467e47a61c9e38090b4c9d2
sheraz-manzoor <sherazmanzoor54@gmail.com>
2024-03-27 16:07:33 +0500

    feat: technologies

--- app/landing-page/_components/code-texhnologies.tsx
+++ app/landing-page/_components/code-texhnologies.tsx
@@ -1,96 +1,134 @@
 import Image from "next/image";
 export default function CodeTechnologies() {
   return (
-    <div className="grid lg:grid-cols-5 items-center mb-12">
-      <div className="flex flex-col gap-4">
+    <div className="grid lg:grid-cols-5 gap-16 items-center mb-12">
+      <div className="image-flex">
         <span className="flex items-center justify-center">
           <Image
-            src={"/word-press-logo.png"}
+            src={"/technologies/wordpress.png"}
             alt="WordPress logo"
-            width={100}
-            height={100}
-            className="w-full h-full"
+            width={200}
+            height={200}
           />
         </span>
         <span className="flex items-center justify-center">
           <Image
-            src={"/mysql-logo.png"}
-            alt="My Sql logo"
-            width={100}
-            height={100}
-            className="w-full h-full"
+            src={"/technologies/mysql.png"}
+            alt="MySql logo"
+            width={200}
+            height={200}
+          />
+        </span>
+        <span className="flex items-center justify-center">
+          <Image
+            src={"/technologies/angular.png"}
+            alt="angular logo"
+            width={200}
+            height={200}
           />
         </span>
       </div>
-      <div className="flex flex-col gap-8">
+      <div className="image-flex">
         <span className="flex items-center justify-center">
           <Image
-            src={"/nodejs-logo.png"}
-            alt="nodejs logo"
-            width={100}
-            height={100}
+            src={"/technologies/nodejs.png"}
+            alt="Nodejs logo"
+            width={200}
+            height={200}
           />
         </span>
         <span className="flex items-center justify-center">
           <Image
-            src={"/codeigniter-logo.png"}
+            src={"/technologies/codeigniter.png"}
             alt="codeigniter logo"
-            width={100}
-            height={100}
+            width={200}
+            height={200}
+          />
+        </span>
+        <span className="flex items-center justify-center">
+          <Image
+            src={"/technologies/reactjs.png"}
+            alt="reactjs logo"
+            width={200}
+            height={200}
           />
         </span>
       </div>
-      <div className="flex flex-col gap-12">
+      <div className="image-flex">
         <span className="flex items-center justify-center">
           <Image
-            src={"/php-logo.png"}
+            src={"/technologies/php.png"}
             alt="php logo"
-            width={100}
-            height={100}
+            width={200}
+            height={200}
           />
         </span>
         <span className="flex items-center justify-center">
           <Image
-            src={"/react-logo.png"}
-            alt="react.js logo"
-            width={180}
-            height={120}
-          />
-        </span>
-      </div>
-      <div className="flex flex-col gap-8">
-        <span className="flex items-center justify-center">
-          <Image
-            src={"/mangodb-logo.png"}
-            alt="php logo"
-            width={100}
-            height={100}
+            src={"/technologies/dumb.png"}
+            alt="dumb logo"
+            width={200}
+            height={200}
           />
         </span>
         <span className="flex items-center justify-center">
           <Image
-            src={"/vue-logo.png"}
-            alt="php logo"
-            width={100}
-            height={100}
+            src={"/technologies/jquery.png"}
+            alt="jquery logo"
+            width={200}
+            height={200}
           />
         </span>
       </div>
-      <div className="flex flex-col gap-8">
+      <div className="image-flex">
         <span className="flex items-center justify-center">
           <Image
-            src={"/bootstap-logo.png"}
-            alt="php logo"
-            width={100}
-            height={100}
+            src={"/technologies/mangodb.png"}
+            alt="mangodb logo"
+            width={200}
+            height={200}
           />
         </span>
         <span className="flex items-center justify-center">
           <Image
-            src={"/vue-logo.png"}
-            alt="php logo"
-            width={100}
-            height={100}
+            src={"/technologies/react-native.png"}
+            alt="react-native logo"
+            width={200}
+            height={200}
+          />
+        </span>
+        <span className="flex items-center justify-center">
+          <Image
+            src={"/technologies/bootstrap.png"}
+            alt="bootstrap logo"
+            width={200}
+            height={200}
+          />
+        </span>
+      </div>
+      <div className="image-flex">
+        <span className="flex items-center justify-center">
+          <Image
+            src={"/technologies/postgresql.png"}
+            alt="postgresql logo"
+            width={200}
+            height={200}
+          />
+        </span>
+        <span className="flex items-center justify-center">
+          <Image
+            src={"/technologies/vue.png"}
+            alt="vue logo"
+            width={200}
+            height={200}
+          />
+        </span>
+        <span className="flex items-center justify-center">
+          <Image
+            src={"/technologies/sass.png"}
+            alt="sass logo"
+            width={200}
+            height={200}
           />
         </span>
       </div>
