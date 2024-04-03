a4d6771526f2f30ebef72b885869788692070f71
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-03 14:46:57 +0500

    chore: updated content

--- app/landing-page/who-we-are.tsx
+++ app/landing-page/who-we-are.tsx
@@ -1,8 +1,11 @@
 import Heading from "../_components/heading";
 import Overselves from "./_components/ourselves";
+import Features from "./_components/features";
+
+
 export default function WhoWeAre() {
   return (
-    <section className="bg-slate-900  px-8 py-4">
+    <section className="bg-slate-900 flex flex-col gap-6 px-8 py-4">
       <div className="flex items-center justify-center">
         <div className="box-max-width">
           <Heading
@@ -12,23 +15,15 @@ export default function WhoWeAre() {
             <span>who</span> we are
           </Heading>
           <p className="main-descriptions text-center">
-            We&apos;re a nationally recognized web design company with a
-            targeted local presence. What does that mean for you? Your site is
-            expertly designed and marketed by professionals who are wizards when
-            it comes to turing users into raving fanatics and generating
-            consistent leads for your business.
-          </p>
-          <p className="main-descriptions text-center mb-4 md:mb-8 lg:mb-10">
-            Design with style provides a full service, in house web design and
-            digital marketing agency with over 13 years of experience in the
-            creative industry. We develop integrated marketing solutions that
-            drive business and boost revenue. We don&apos;t need to outsource
-            your work overseas or to robots. We are real people, with a real
-            passion for what we do.
+            At Infinity Devs, we are passionate about crafting cutting-edge software solutions that exceed expectations.
+            Our dedicated team of experienced developers and designers collaborate seamlessly to bring your vision to 
+            life with exceptional precision and creativity. Together, we strive to create innovative web apps, MVPs, landing pages, 
+            and proof of concepts, driving your success with top-notch code quality and design expertise.
           </p>
         </div>
       </div>
-      <Overselves />
+      <Features />
+      {/* <Overselves /> */}
     </section>
   );
 }
