980fef6a1eedfcb752da76a5fcf2e7f634119549
SyedSabee <sabishah352@gmail.com>
2024-05-01 21:57:56 +0500

    straps marquee tag remove

--- app/landing-page/_components/straps.tsx
+++ app/landing-page/_components/straps.tsx
@@ -2,23 +2,20 @@ export default function Straps() {
   return (
     <div className="mt-8 w-full">
       <div className="relative w-full">
-        <marquee className="strap justify-between">
-          <span className="px-20">web design</span>
-          <span className="px-20">web apps</span>
-          <span className="px-20">seo</span>
-          <span className="px-20">mobile apps</span>
-          <span className="px-20">web development</span>
-        </marquee>
-        <marquee
-          className="strap absolute  top-0 left-0 shadow-lg -rotate-3 flex justify-around"
-          direction="right"
-        >
-          <span className="px-20">ui/ux design</span>
-          <span className="px-20">Mobile apps</span>
-          <span className="px-20">web development</span>
-          <span className="px-20">graphic design</span>
+        <div className="strap justify-between">
+          <span>web design</span>
+          <span>web apps</span>
           <span>seo</span>
-        </marquee>
+          <span>mobile apps</span>
+          <span>web development</span>
+        </div>
+        <div className="strap absolute  top-0 left-0 shadow-lg -rotate-3 flex justify-around">
+          <span>ui/ux design</span>
+          <span>Mobile apps</span>
+          <span>web development</span>
+          <span>graphic design</span>
+          <span>seo</span>
+        </div>
       </div>
     </div>
   );
