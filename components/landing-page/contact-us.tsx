594787713831d24fd7049a5ef54647c7242f54f6
sheraz4196 <sheraztoor321@gmail.com>
2024-05-23 23:33:12 +0500

    refactor: separate folder for forms

--- components/landing-page/contact-us.tsx
+++ components/landing-page/contact-us.tsx
@@ -1,10 +1,10 @@
-import ContactForm from "./_components/contact-form";
+import ContactForm from "../forms/contact-form";
 
 export default function ContactUs() {
   return (
     <section
-      id='contact-us'
-      className='px-5 py-8 mx-auto md:px-8 md:py-12 max-w-7xl mx-auto'
+      id="contact-us"
+      className="px-5 py-8 md:px-8 md:py-12 max-w-7xl mx-auto"
     >
       <ContactForm />
     </section>
