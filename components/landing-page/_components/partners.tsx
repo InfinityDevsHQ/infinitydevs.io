46b664daffb693191ba01b4d9aed5e2661b12bdd
sheraz4196 <sheraztoor321@gmail.com>
2024-05-23 15:15:44 +0500

    refactor: improved our partners code

--- components/landing-page/_components/partners.tsx
+++ components/landing-page/_components/partners.tsx
@@ -1,40 +1,41 @@
 import Image from "next/image";
+import { Fragment } from "react";
 
 export default function Partners() {
+  const Partners = [
+    {
+      imgUrl: "/partners/partner-1.png",
+      alt: "Partner-1",
+    },
+    {
+      imgUrl: "/partners/partner-2.png",
+      alt: "Partner-2",
+    },
+    {
+      imgUrl: "/partners/partner-3.png",
+      alt: "Partner-3",
+    },
+    {
+      imgUrl: "/partners/partner-4.png",
+      alt: "Partner-4",
+    },
+  ];
   return (
     <div className="flex flex-col lg:flex-row gap-12 items-center justify-between">
-      <span>
-        <Image
-          src={"/partners/partner-1.png"}
-          alt=""
-          width={200}
-          height={200}
-        />
-      </span>
-      <span>
-        <Image
-          src={"/partners/partner-2.png"}
-          alt=""
-          width={200}
-          height={200}
-        />
-      </span>
-      <span>
-        <Image
-          src={"/partners/partner-3.png"}
-          alt=""
-          width={200}
-          height={200}
-        />
-      </span>
-      <span>
-        <Image
-          src={"/partners/partner-4.png"}
-          alt=""
-          width={200}
-          height={200}
-        />
-      </span>
+      {Partners.map((partner, index) => {
+        return (
+          <Fragment key={index}>
+            <span>
+              <Image
+                src={partner.imgUrl}
+                alt={partner.alt}
+                width={200}
+                height={200}
+              />
+            </span>
+          </Fragment>
+        );
+      })}
     </div>
   );
 }
