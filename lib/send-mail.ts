b241d40b27a5696461652cde49522b65fb1c7026
Saad Gulzar <isaadgulzar@gmail.com>
2024-06-07 01:42:30 +0500

    fix: fix email message and reset form after submit

--- lib/send-mail.ts
+++ lib/send-mail.ts
@@ -6,6 +6,7 @@ const NEXT_PUBLIC_SENDER_EMAIL_PASSWORD =
 const NEXT_PUBLIC_RECIPIENT_EMAIL = process.env.NEXT_PUBLIC_RECIPIENT_EMAIL;
 const NEXT_PUBLIC_SENDER_EMAIL = process.env.NEXT_PUBLIC_SENDER_EMAIL;
 const NEXT_PUBLIC_FROM_EMAIL = process.env.NEXT_PUBLIC_FROM_EMAIL;
+
 const transporter = nodemailer.createTransport({
   host: NEXT_PUBLIC_SMTP_HOST,
   port: 587,
@@ -17,21 +18,16 @@ const transporter = nodemailer.createTransport({
 });
 
 export async function sendMail({
-  email,
   subject,
   text,
   html,
   sendTo,
 }: {
-  email: string;
   subject: string;
   text?: string;
   html?: string;
   sendTo?: string;
 }) {
-  console.log(
-    `UserName: ${NEXT_PUBLIC_SENDER_EMAIL}\nPassword:${NEXT_PUBLIC_SENDER_EMAIL_PASSWORD}\nHost: ${NEXT_PUBLIC_SMTP_HOST}`
-  );
   try {
     const info = await transporter.sendMail({
       from: NEXT_PUBLIC_FROM_EMAIL,
@@ -40,9 +36,7 @@ export async function sendMail({
       text: text,
       html: html || "",
     });
-
     console.log("Message Sent", info.messageId);
-    console.log("Mail sent to", NEXT_PUBLIC_RECIPIENT_EMAIL);
     return info;
   } catch (error) {
     console.error("Error sending email", error);
