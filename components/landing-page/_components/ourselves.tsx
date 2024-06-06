7173524c809280cc74d10ec5aa0e680b9827e72d
Saad Gulzar <isaadgulzar@gmail.com>
2024-06-07 03:52:45 +0500

    fix: fix ui design for large screens

--- components/landing-page/_components/ourselves.tsx
+++ components/landing-page/_components/ourselves.tsx
@@ -1,61 +1,61 @@
 import Card from "$/components/card";
 import Image from "next/image";
 export default function AboutUs() {
-  return (
-    <div className="grid md:grid-cols-3 gap-4 md:gap-6 lg:gap-10 max-w-7xl mx-auto xl:px-20">
-      <Card
-        icon={
-          <Image
-            src={"/cards-images/arrow.png"}
-            width={164}
-            height={164}
-            alt="Scope"
-            className="w-20 h-20 lg:w-40 lg:h-40"
-          />
-        }
-        title="Our Mission"
-        className={"bg-purple-900/20"}
-      >
-        Our vision at Infinity Devs is to continuously innovate and push the
-        boundaries in software, design, and AI solutions to create endless
-        possibilities.
-      </Card>
+	return (
+		<div className="grid md:grid-cols-3 gap-4 md:gap-6 lg:gap-10 max-w-screen-2xl mx-auto xl:px-20">
+			<Card
+				icon={
+					<Image
+						src={"/cards-images/arrow.png"}
+						width={164}
+						height={164}
+						alt="Scope"
+						className="w-20 h-20 lg:w-40 lg:h-40"
+					/>
+				}
+				title="Our Mission"
+				className={"bg-purple-900/20"}
+			>
+				Our vision at Infinity Devs is to continuously innovate and push the
+				boundaries in software, design, and AI solutions to create endless
+				possibilities.
+			</Card>
 
-      <Card
-        icon={
-          <Image
-            src={"/cards-images/scope.png"}
-            width={164}
-            height={164}
-            alt="Scope"
-            className="w-20 h-20 lg:w-40 lg:h-40"
-          />
-        }
-        title="Our Vision"
-        className={" bg-slate-800/80"}
-      >
-        Our vision at Infinity Devs is to continuously innovate and push the
-        boundaries in software, design, and AI solutions to create endless
-        possibilities.
-      </Card>
+			<Card
+				icon={
+					<Image
+						src={"/cards-images/scope.png"}
+						width={164}
+						height={164}
+						alt="Scope"
+						className="w-20 h-20 lg:w-40 lg:h-40"
+					/>
+				}
+				title="Our Vision"
+				className={" bg-slate-800/80"}
+			>
+				Our vision at Infinity Devs is to continuously innovate and push the
+				boundaries in software, design, and AI solutions to create endless
+				possibilities.
+			</Card>
 
-      <Card
-        icon={
-          <Image
-            src={"/cards-images/settings.png"}
-            width={164}
-            height={164}
-            alt="Settings"
-            className="w-20 h-20 lg:w-40 lg:h-40"
-          />
-        }
-        title="Our Strategy"
-        className={"bg-purple-900/20"}
-      >
-        Our strategy at Infinity Devs centers around collaboration, effective
-        communication, and ongoing learning to ensure we stay at the forefront
-        of digital trends and technologies.
-      </Card>
-    </div>
-  );
+			<Card
+				icon={
+					<Image
+						src={"/cards-images/settings.png"}
+						width={164}
+						height={164}
+						alt="Settings"
+						className="w-20 h-20 lg:w-40 lg:h-40"
+					/>
+				}
+				title="Our Strategy"
+				className={"bg-purple-900/20"}
+			>
+				Our strategy at Infinity Devs centers around collaboration, effective
+				communication, and ongoing learning to ensure we stay at the forefront
+				of digital trends and technologies.
+			</Card>
+		</div>
+	);
 }
--- components/landing-page/_components/reviews-gallery.tsx
+++ components/landing-page/_components/reviews-gallery.tsx
@@ -1,149 +1,152 @@
 import React from "react";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
+	Carousel,
+	CarouselContent,
+	CarouselItem,
 } from "$/components/ui/carousel";
 
 import Review from "./review";
 const ReviewData = [
-  {
-    imgUrl: "/user.svg",
-    review: `Very clean and professional website built in Next.js. Was built and delivered quickly with the occasional updates on the progress. Any requests for changes or updates was made and made quickly. Would definitely use again in the future for anything React related.`,
-    clientName: "tylerjjankowski",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Outstanding work. They worked fast and were happy to fix problems in a revision which I initially didn't communicated. I am a very happy customer.`,
-    clientName: "wintermeyer",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Great communication, I highly recommend working with them, tremendously talented and should be at the top of your list when it comes to web development!`,
-    clientName: "johnmctighe",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `They did an amazing job on the Tailwind templates I asked them to put together. They went above and beyond in a number of ways such as adding smooth little animations and overall polish stuff that we had not even considered. Definitely going to be considering them for all future projects.`,
-    clientName: "brettlawson276",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Very responsive and the quality of the work is excellent.`,
-    clientName: "idanvelleman",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Great communication, I highly recommend working with them, tremendously talented and should be at the top of your list when it comes to web development!`,
-    clientName: "johnmctighe",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `They did an amazing job on the Tailwind templates I asked them to put together. They went above and beyond in a number of ways such as adding smooth little animations and overall polish stuff that we had not even considered. Definitely going to be considering them for all future projects.`,
-    clientName: "brettlawson276",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Very responsive and the quality of the work is excellent.`,
-    clientName: "idanvelleman",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Great communication, I highly recommend working with them, tremendously talented and should be at the top of your list when it comes to web development!`,
-    clientName: "johnmctighe",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Very responsive and the quality of the work is excellent.`,
-    clientName: "idanvelleman",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
-  {
-    imgUrl: "/user.svg",
-    review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
-    clientName: "voguetyre",
-  },
+	{
+		imgUrl: "/user.svg",
+		review: `Very clean and professional website built in Next.js. Was built and delivered quickly with the occasional updates on the progress. Any requests for changes or updates was made and made quickly. Would definitely use again in the future for anything React related.`,
+		clientName: "tylerjjankowski",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Outstanding work. They worked fast and were happy to fix problems in a revision which I initially didn't communicated. I am a very happy customer.`,
+		clientName: "wintermeyer",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Great communication, I highly recommend working with them, tremendously talented and should be at the top of your list when it comes to web development!`,
+		clientName: "johnmctighe",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `They did an amazing job on the Tailwind templates I asked them to put together. They went above and beyond in a number of ways such as adding smooth little animations and overall polish stuff that we had not even considered. Definitely going to be considering them for all future projects.`,
+		clientName: "brettlawson276",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Very responsive and the quality of the work is excellent.`,
+		clientName: "idanvelleman",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Great communication, I highly recommend working with them, tremendously talented and should be at the top of your list when it comes to web development!`,
+		clientName: "johnmctighe",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `They did an amazing job on the Tailwind templates I asked them to put together. They went above and beyond in a number of ways such as adding smooth little animations and overall polish stuff that we had not even considered. Definitely going to be considering them for all future projects.`,
+		clientName: "brettlawson276",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Very responsive and the quality of the work is excellent.`,
+		clientName: "idanvelleman",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Great communication, I highly recommend working with them, tremendously talented and should be at the top of your list when it comes to web development!`,
+		clientName: "johnmctighe",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Very responsive and the quality of the work is excellent.`,
+		clientName: "idanvelleman",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
+	{
+		imgUrl: "/user.svg",
+		review: `Exceptional work, on time and at a very reasonable price. I would highly recommend to anyone.`,
+		clientName: "voguetyre",
+	},
 ];
 export default function ReviewsGallery() {
-  return (
-    <div className="flex items-center justify-center black-fade max-w-7xl mx-auto">
-      <div className="lg:grid lg:grid-cols-4 gap-4 hidden">
-        {[...Array(4)].map((_, colIndex) => (
-          <div key={colIndex}>
-            {ReviewData.slice(colIndex * 4, colIndex * 5 + 5).map(
-              (review, index) => (
-                <div key={index} className="flex flex-col gap-4 mt-5">
-                  <Review
-                    imageUrl={review.imgUrl}
-                    name={review.clientName}
-                    review={review.review}
-                  />
-                </div>
-              )
-            )}
-          </div>
-        ))}
-      </div>
-      <Carousel
-        className="max-w-full lg:hidden"
-        opts={{
-          loop: true,
-        }}
-      >
-        <CarouselContent>
-          {ReviewData.map((review, index) => (
-            <CarouselItem
-              key={index}
-              className="flex justify-center items-center basis-4/5  gAp-16"
-            >
-              <Review
-                review={review.review}
-                name={review.clientName}
-                imageUrl={review.imgUrl}
-              />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center black-fade max-w-screen-2xl mx-auto">
+			<div className="lg:grid lg:grid-cols-4 gap-4 hidden">
+				{[...Array(4)].map((_, colIndex) => (
+					<div key={colIndex}>
+						{ReviewData.slice(colIndex * 4, colIndex * 5 + 5).map(
+							(review, index) => (
+								<div
+									key={index}
+									className="flex flex-col gap-4 mt-5"
+								>
+									<Review
+										imageUrl={review.imgUrl}
+										name={review.clientName}
+										review={review.review}
+									/>
+								</div>
+							)
+						)}
+					</div>
+				))}
+			</div>
+			<Carousel
+				className="max-w-full lg:hidden"
+				opts={{
+					loop: true,
+				}}
+			>
+				<CarouselContent>
+					{ReviewData.map((review, index) => (
+						<CarouselItem
+							key={index}
+							className="flex justify-center items-center basis-4/5  gAp-16"
+						>
+							<Review
+								review={review.review}
+								name={review.clientName}
+								imageUrl={review.imgUrl}
+							/>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+			</Carousel>
+		</div>
+	);
 }
