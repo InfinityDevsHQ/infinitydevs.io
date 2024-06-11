f03a449c9b5a0ae17fc13bc37425da3bb0bf3366
sheraz4196 <sheraztoor321@gmail.com>
2024-06-11 14:58:46 +0500

    feat: filters pills

--- components/landing-page/portfolio.tsx
+++ components/landing-page/portfolio.tsx
@@ -2,29 +2,38 @@ import Heading from "../../app/_components/heading";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import PortfolioGrid from "./_components/portfolio-grid";
+import { Checkbox } from "../ui/checkbox";
 export default function Portfolio() {
-	return (
-		<section className="bg-slate-900">
-			<div className="px-5 py-8 md:py-12 md:px-8 xl:px-20 max-w-screen-2xl mx-auto">
-				<div className="flex items-center justify-center">
-					<div>
-						<Heading
-							size="h2"
-							className="uppercase mb-3 md:mb-6 lg:mb-8 font-bold text-center"
-						>
-							<span>our</span> portfolio
-						</Heading>
-						<Link
-							href={"/"}
-							className="flex common-border max-w-max mx-auto items-center justify-between px-2 py-1 mb-3 md:mb-6 lg:mb-8 text-sm text-zinc-300 font-semibold bg-blue-900/40 rounded-full"
-						>
-							<span>What Others Say About Us</span>
-							<ArrowRight size={20} />
-						</Link>
-					</div>
-				</div>
-				<PortfolioGrid />
-			</div>
-		</section>
-	);
+  const Filters = [
+    "Logo Design",
+    "UI/UX Design",
+    "Tailwind CSS",
+    "React.js",
+    "Next.js",
+    "Vue.js",
+  ];
+  return (
+    <section className="bg-slate-900">
+      <div className="px-5 py-8 md:py-12 md:px-8 xl:px-20 max-w-screen-2xl mx-auto">
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col gap-12 mb-12">
+            <Heading
+              size="h2"
+              className="uppercase mb-3 md:mb-6 lg:mb-8 font-bold text-center"
+            >
+              <span>our</span> portfolio
+            </Heading>
+            <div className="flex items-center gap-6">
+              {Filters.map((filter, index) => (
+                <Checkbox key={index} pill>
+                  {filter}
+                </Checkbox>
+              ))}
+            </div>
+          </div>
+        </div>
+        <PortfolioGrid />
+      </div>
+    </section>
+  );
 }
--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,30 +1,79 @@
-"use client"
-
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
-
-import { cn } from "$/lib/utils"
+"use client";
+import { cn } from "$/lib/utils";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+import * as React from "react";
+interface CheckboxProps
+  extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
+  pill?: boolean;
+  children?: React.ReactNode;
+}
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-zinc-200 border-zinc-900 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-zinc-900 data-[state=checked]:text-zinc-50 dark:border-zinc-800 dark:border-zinc-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300 dark:data-[state=checked]:bg-zinc-50 dark:data-[state=checked]:text-zinc-900",
-      className
-    )}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
-      <Check className="h-4 w-4" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+  CheckboxProps
+>(({ className, pill = false, children, ...props }, ref) => {
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <>
+      {!pill ? (
+        <CheckboxPrimitive.Root
+          ref={ref}
+          onCheckedChange={(state) => setChecked(state === true)}
+          className={cn(
+            "border-border-gray-300 peer h-8 w-8 rounded-md border bg-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+            className
+          )}
+          {...props}
+        >
+          <CheckboxPrimitive.Indicator
+            className={cn("flex items-center justify-center fill-current")}
+          >
+            <Check className="h-4 w-4 text-black" />
+          </CheckboxPrimitive.Indicator>
+        </CheckboxPrimitive.Root>
+      ) : (
+        <div
+          className={cn(
+            "flex max-w-max items-center gap-2 p-2 px-3 text-base",
+            checked ? "bg-primary" : "bg-white"
+          )}
+          style={{
+            boxShadow: "0px 4px 50px rgba(0, 0, 0, 0.05)",
+            borderRadius: "100px",
+          }}
+        >
+          <CheckboxPrimitive.Root
+            ref={ref}
+            onCheckedChange={(state) => setChecked(state === true)}
+            className={cn(
+              "peer h-8 w-8 rounded-md bg-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+              className
+            )}
+            {...props}
+          >
+            <span
+              className={cn(
+                "flex h-6 w-6 items-center justify-center rounded-full",
+                checked ? "bg-white" : "bg-mute-2"
+              )}
+            >
+              <Check
+                className={cn(
+                  "h-3 w-3 text-black",
+                  checked ? "text-primary" : "text-mute-1"
+                )}
+              />
+            </span>
+          </CheckboxPrimitive.Root>
+          <div className={cn(checked ? "text-white" : "text-mute-1")}>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+});
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox }
+export { Checkbox };
