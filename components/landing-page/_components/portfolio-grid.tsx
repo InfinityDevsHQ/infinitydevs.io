b74c2739b29e3bff9175798be941fa99a3383b5b
sheraz4196 <sheraztoor321@gmail.com>
2024-06-11 15:02:46 +0500

    fix: portfolio grid layout

--- components/landing-page/_components/portfolio-grid.tsx
+++ components/landing-page/_components/portfolio-grid.tsx
@@ -2,7 +2,7 @@ import PortfolioCard from "./portfolio-card";
 export default function PortfolioGrid() {
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="lg:mb-12 grid lg:grid-cols-3 gap-12 items-center justify-center justify-items-center w-full">
+      <div className="lg:mb-12 grid lg:grid-cols-3 gap-12 justify-center justify-items-center w-full">
         <PortfolioCard
           imageUrl="/portfolio/portfolio-1.png"
           tag="Graphic Design"
